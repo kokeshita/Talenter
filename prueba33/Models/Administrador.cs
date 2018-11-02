887e3dbb839785e49fc0059230c4d0eae10b8f08
kokeshita <44687465+kokeshita@users.noreply.github.com>
2018-11-01 22:18:19 -0300

    Update Administrador.cs

--- prueba33/Models/Administrador.cs
+++ prueba33/Models/Administrador.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 namespace prueba33.Models
 {
@@ -7,8 +7,30 @@ namespace prueba33.Models
         public string RazonSocial { get; set; }
         public List<Empleado> Empleados { get; set; }
         
-        public Administrador()
+        public Administrador(string RazonSocial)
         {
+            this.RazonSocial = RazonSocial;
+            Empleados = new List<Empleado>();
+        }
+
+        public Administrador(string RazonSocial, List<Empleado> Empleados) {
+            this.RazonSocial = RazonSocial;
+            this.Empleados = Empleados;
+        }
+
+        public void darAltaEmpleado(Empleado empleado) {
+            Empleados.Add(empleado);
+        }
+
+        public void darBajaEmpleado(Empleado empleado) {
+            Empleados.Remove(empleado);
+        }
+
+        public void verEmpleado(Empleado empleado){
+            //TODO: Ver bien como hacer esto
+            if(Empleados.Contains(empleado)) {
+                //
+            }
         }
     }
 }
