ef348f5da59d0fe1aa7d3faef7014dcc31f18ab0
kokeshita <44687465+kokeshita@users.noreply.github.com>
2018-11-01 22:19:09 -0300

    Update Busqueda.cs

--- prueba33/Models/Busqueda.cs
+++ prueba33/Models/Busqueda.cs
@@ -1,18 +1,27 @@
-﻿using System;
+using System;
 namespace prueba33.Models
 {
     public class Busqueda
     {
-        public string Puesto { get; set; } //TODO; Reemplazar por Puesto
+        public string Puesto { get; set; } //TODO; Reemplazar por Puesto?
         public float Salario { get; set; }
-        public string Rubro { get; set; } //TODO: Cambiar por Rubro
-        public string Modalidad { get; set; } //TODO: Cambiar por Enum Modalidad
-        public string Contrato { get; set; } //TODO: Cambio Enum TipoContrato
-        public string Dedicacion { get; set; } //TODO; Cambiar Enum Dedicacion
+        public Rubro Rubro { get; set; }
+        public Modalidad Modalidad { get; set; } 
+        public TipoContrato Contrato { get; set; }
+        public Dedicacion Dedicacion { get; set; }
         public string Descripcion { get; set; }
 
-        public Busqueda()
+        public Busqueda(string Puesto, Rubro Rubro, TipoContrato Contrato, string Descripcion)
         {
+            this.Puesto = Puesto;
+            this.Rubro = Rubro;
+            this.Contrato = Contrato;
+            this.Descripcion = Descripcion;
+        }
+
+        public Busqueda(string Puesto, Rubro rubro){
+            this.Puesto = Puesto;
+            this.Rubro = Rubro;
         }
     }
 }
